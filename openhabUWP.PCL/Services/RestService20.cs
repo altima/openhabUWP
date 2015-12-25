96b730779e9cc772fa0d651f7e18debd518e97ec
Altima <enrico@soapbubbles.de>
2015-12-25 23:44:40 +0100

    added parameter for topics

--- openhabUWP.PCL/Services/RestService20.cs
+++ openhabUWP.PCL/Services/RestService20.cs
@@ -107,17 +107,19 @@ namespace openhabUWP.Services
         /// by http://danielwertheim.se/2013/09/15/using-c-and-httpclient-to-consume-continuously-streamed-results/
         /// </summary>
         /// <param name="baseUrl">The base URL.</param>
+        /// <param name="topcis">The topcis [default: [smarthome,items,*,state]]</param>
         /// <param name="onDataReceived">The on data received.</param>
         /// <param name="onEventReceived">The on event received.</param>
         /// <returns></returns>
-        public async Task AttachToEvents(string baseUrl, Action<string> onDataReceived = null, Action<string> onEventReceived = null)
+        public async Task AttachToEvents(string baseUrl, string[] topcis = null, Action<string> onDataReceived = null, Action<string> onEventReceived = null)
         {
             //defaults
             if (onDataReceived == null) onDataReceived = (input) => { };
             if (onEventReceived == null) onEventReceived = (input) => { };
-
+            if (topcis == null) topcis = new string[] { "smarthome", "items", "*", "state" };
+            
             //build uri
-            string url = baseUrl + "/events";
+            string url = string.Concat(baseUrl, "/events?topics=", string.Join("/", topcis));
             using (var client = new HttpClient())
             {
                 client.Timeout = TimeSpan.FromMilliseconds(Timeout.Infinite);
