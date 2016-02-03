baaea2ae3d5cd130f86f5d300a431e856e0525b4
Altima <mail@enricomeinel.de>
2016-02-03 23:24:54 +0100

    updated gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,39 @@ module.exports = function(grunt) {
 				options: {
 					projectConfiguration: 'Release',
 					targets: ['Clean', 'Rebuild'],
+					platform: 'ARM',
+					version: 14.0,
+					maxCpuCount: 2,
+					buildParameters: {
+						WarningLevel: 2
+					},
+					verbosity: 'quiet',
+					AppxBundle: 'Always',
+					AppxBundlePlatforms: 'x86|x64|ARM'
+				}
+			},
+			x86: {
+				src: ['openhabUWP.sln'],
+				options: {
+					projectConfiguration: 'Release',
+					targets: ['Clean', 'Rebuild'],
+					platform: 'x86',
+					version: 14.0,
+					maxCpuCount: 2,
+					buildParameters: {
+						WarningLevel: 2
+					},
+					verbosity: 'quiet',
+					AppxBundle: 'Always',
+					AppxBundlePlatforms: 'x86|x64|ARM'
+				}
+			},
+			arm: {
+				src: ['openhabUWP.sln'],
+				options: {
+					projectConfiguration: 'Release',
+					targets: ['Clean', 'Rebuild'],
+					platform: 'ARM',
 					version: 14.0,
 					maxCpuCount: 2,
 					buildParameters: {
@@ -26,5 +59,5 @@ module.exports = function(grunt) {
 	});
 	grunt.loadNpmTasks('grunt-msbuild');
 	grunt.loadNpmTasks('grunt-nuget');
-	grunt.registerTask('default', ['nugetrestore', 'msbuild']);
+	grunt.registerTask('default', ['nugetrestore', 'msbuild:arm', 'msbuild:x86']);
 };
